7f407052fedc8582a4b71a59046b20c93a983bd3
MyGit <you@example.com>
2022-12-04 23:39:31 +0100

    the last version

--- Downloading_Playlist.py
+++ Downloading_Playlist.py
@@ -17,23 +17,18 @@ from pytube import YouTube
 from pytube import Playlist
 
 path="Y:\CoursesUniversity\BBB"
-playlist = Playlist('https://www.youtube.com/watch?v=zez2Tv-bcXY&list=PL9ooVrP1hQOFrYxqxb0NJCdCABPZNo0pD')
+playlist = Playlist('https://www.youtube.com/watch?v=Te67sBlJJMg&list=PLD4dEalVnw4U2YVu2P79dQ_i_eTQ4N-O8')
 print('Number of videos in playlist: %s' % len(playlist.video_urls))
 
-
-
-
-
-
-def Download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+for video in playlist.video_urls:
+    print('le nom de videoest:',video)
+    youtubeObject = YouTube(video)
+    youtubeObject=youtubeObject.streams.get_highest_resolution()
     try:
-        youtubeObject.download()
+        youtubeObject.download(path)
     except:
         print("Error found")
     print("Download is completed successfully")
+    
 
 
-link = input("Enter the YouTube video URL: ")
-Download(link)
