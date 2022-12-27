ecc8546563c0ce81d3b6b288eab4a71d0730fb0a
Youssef KABIR <59072693+Youssefkabbir@users.noreply.github.com>
2022-12-27 15:37:25 +0100

    Update Downloading_Playlist.py

--- Downloading_Playlist.py
+++ Downloading_Playlist.py
@@ -1,34 +1,22 @@
 
-# =============================================================================
-# from pytube import Playlist
-# path="Y:\CoursesUniversity\Big_Data"
-# playlist = Playlist('https://www.youtube.com/watch?v=zez2Tv-bcXY&list=PL9ooVrP1hQOFrYxqxb0NJCdCABPZNo0pD')
-# print('Number of videos in playlist: %s' % len(playlist.video_urls))
-# 
-# # Loop through all videos in the playlist and download them
-# for video in playlist.videos:
-#     print('le nom de videoest:',video)
-#     video.streams.first().download(path)
-# =============================================================================
-    
-########################################"
-##########################################
+
 from pytube import YouTube
 from pytube import Playlist
-
+#here we give the path where the playlist video will be saved
 path="Y:\CoursesUniversity\BBB"
+# here we give the path of the playlist
 playlist = Playlist('https://www.youtube.com/watch?v=Te67sBlJJMg&list=PLD4dEalVnw4U2YVu2P79dQ_i_eTQ4N-O8')
-print('Number of videos in playlist: %s' % len(playlist.video_urls))
+print('the Urls of  videos in playlist: %s' % len(playlist.video_urls))
 
 for video in playlist.video_urls:
-    print('le nom de videoest:',video)
     youtubeObject = YouTube(video)
     youtubeObject=youtubeObject.streams.get_highest_resolution()
     try:
         youtubeObject.download(path)
     except:
         print("Error found")
-    print("Download is completed successfully")
+ 
+print("Download is completed successfully")
     
 
 
